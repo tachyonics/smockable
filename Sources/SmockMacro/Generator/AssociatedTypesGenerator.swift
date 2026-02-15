658151503a69ba8fa323b1c9b26c3d26dcac237b
tachyonics <simon@tachyonics.io>
2026-02-15 21:20:04 +1100

    Add standard header to all files. (#57)

--- Sources/SmockMacro/Generator/AssociatedTypesGenerator.swift
+++ Sources/SmockMacro/Generator/AssociatedTypesGenerator.swift
@@ -1,6 +1,20 @@
+//===----------------------------------------------------------------------===//
 //
-//  AssociatedTypes.swift
-//  smockable
+// This source file is part of the Smockable open source project
+//
+// Copyright (c) 2026 the Smockable authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Smockable authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+//
+//  AssociatedTypesGenerator.swift
+//  SmockMacro
 //
 
 import SwiftSyntax
